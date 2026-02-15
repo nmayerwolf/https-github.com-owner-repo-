fea6ffd9260959b73a39092027bc36558a1d6aae
Nicolas Mayer Wolf <nmayerwolf@MacBook-Air-2025.local>
2026-02-15 12:02:29 -0300

    refactor(phase4): scaffold monorepo workspaces with shared @nexusfin/core

---
+++ packages/nexusfin-core/index.cjs
@@ -0,0 +1,7 @@
+const { calculateIndicators } = require('./analysis.cjs');
+const { calculateConfluence } = require('./confluence.cjs');
+
+module.exports = {
+  calculateIndicators,
+  calculateConfluence
+};
